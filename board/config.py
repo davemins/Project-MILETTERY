82229f9f2d4cfc3336b493c6643742532ec9228f
davemins <davemins4125@naver.com>
2023-05-24 14:14:37 +0900

    access success

---
+++ board/config.py
@@ -0,0 +1,6 @@
+name = '홍길동'
+bday = 20010101
+edate = 20210607
+uname = '육군훈련소'
+id = 'someone@example.com'
+pw = 'q1w2e3r4!'
--- milettery/settings.py
+++ milettery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-zicu&tubx=(b)aq7wj-ziyn=hqeef15c3-z=7i^46nv-hqtvcn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-KR"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
